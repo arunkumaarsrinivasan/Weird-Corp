54abb493130b6b0c963c64b562fe65299834fe97
arunkumaarsrinivasan <51884745+arunkumaarsrinivasan@users.noreply.github.com>
2019-06-30 21:12:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+   <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    <h1>Dinosaur</h1>
+    </div>
+  );
+}
+
+export default App;
